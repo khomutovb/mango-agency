0f3a6dfb821fe2e774b7830110f6b75a7b97b5a0
khomutovb <48166365+khomutovb@users.noreply.github.com>
2020-10-16 16:59:58 +0300

    Add files via upload

---
+++ js/project-script.js
@@ -0,0 +1,27 @@
+let t = document.getElementById("cursor");
+document.querySelector(".section-row").addEventListener("mousemove", function (n) {
+/*     t.style.left = n.clientX + "px", 
+    t.style.top = n.clientY + "px" */
+    translateX = n.clientX - 150;
+    translateY = n.clientY - 150;
+    t.style.transform = 'translateX(' + translateX + 'px) translateY(' + translateY + 'px) rotate(' + n.clientX + 'deg)';
+});
+function n() {
+    t.classList.add("hover")
+}
+function s() {
+    t.classList.remove("hover")
+}
+function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+}
+document.addEventListener('DOMContentLoaded', function () {
+    let hoverLink = document.querySelectorAll("a.menu__item");
+    for (let i = 0; i < hoverLink.length; i++) {
+        o(hoverLink[i]);
+        let url = hoverLink[i].getAttribute("data-img");
+        hoverLink[i].addEventListener('mouseenter', function () {
+            t.style.backgroundImage = "url(" + url + ")";
+        });
+    }
+})
